28316b4bcd4ad5f4f0356a58ad1486730cde72a0
Devil_king <88047360+prabithkuttu@users.noreply.github.com>
2021-08-16 21:59:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('LiyaMol bot working perfectly 👻')
+            chalk.green.bold('Kurumbi bot working perfectly 👻')
         );
     });
     
